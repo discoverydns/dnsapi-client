6716e66a101ac1bcbf46eef7b2781d09e8003daf
Chris Wright <chris@ausregistry.com.au>
2014-02-05 10:54:36 +1100

    fixed trust store bug

--- src/main/java/com/discoverydns/dnsapiclient/internal/DefaultSSLContextFactory.java
+++ src/main/java/com/discoverydns/dnsapiclient/internal/DefaultSSLContextFactory.java
@@ -11,37 +11,28 @@ import javax.net.ssl.TrustManagerFactory;
 
 import org.eclipse.jetty.util.resource.Resource;
 
-import com.discoverydns.dnsapiclient.config.DefaultSSLContextFactoryConfig;
 import com.discoverydns.dnsapiclient.SSLContextFactory;
+import com.discoverydns.dnsapiclient.config.DefaultSSLContextFactoryConfig;
 
 public class DefaultSSLContextFactory implements SSLContextFactory {
 
 	private static final String PROTOCOL_VERSION = "TLSv1.1";
 
-    private final DefaultSSLContextFactoryConfig sslConfig;
+	private final DefaultSSLContextFactoryConfig sslConfig;
 
-    public DefaultSSLContextFactory(DefaultSSLContextFactoryConfig sslConfig) {
-        this.sslConfig = sslConfig;
-    }
+	public DefaultSSLContextFactory(DefaultSSLContextFactoryConfig sslConfig) {
+		this.sslConfig = sslConfig;
+	}
 
 	@Override
-	public SSLContext createSSLContext()
-			throws Exception {
+	public SSLContext createSSLContext() throws Exception {
 
-		InputStream keyStoreIS = Resource.newResource(sslConfig.getKeyStorePath())
-				.getInputStream();
+		InputStream keyStoreIS = Resource.newResource(
+				sslConfig.getKeyStorePath()).getInputStream();
 
-		final KeyStore keyStore = KeyStore
-				.getInstance(sslConfig.getKeyStoreType());
+		final KeyStore keyStore = KeyStore.getInstance(sslConfig
+				.getKeyStoreType());
 		keyStore.load(keyStoreIS, sslConfig.getKeyStorePassword().toCharArray());
-
-		InputStream trustStoreIS = Resource.newResource(
-                sslConfig.getTrustStorePath()).getInputStream();
-
-		final KeyStore trustStore = KeyStore.getInstance(sslConfig
-				.getTrustStoreType());
-		trustStore.load(trustStoreIS, sslConfig.getTrustStorePassword()
-				.toCharArray());
 
 		final KeyManagerFactory keyManagerFactory = KeyManagerFactory
 				.getInstance(KeyManagerFactory.getDefaultAlgorithm());
@@ -49,11 +40,22 @@ public class DefaultSSLContextFactory implements SSLContextFactory {
 				.toCharArray());
 		final KeyManager keyManagers[] = keyManagerFactory.getKeyManagers();
 
-		final TrustManagerFactory trustManagerFactory = TrustManagerFactory
-				.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-		trustManagerFactory.init(trustStore);
-		final TrustManager trustManagers[] = trustManagerFactory
-				.getTrustManagers();
+		TrustManager trustManagers[] = null;
+		if (sslConfig.getTrustStorePath() != null) {
+			InputStream trustStoreIS = Resource.newResource(
+					sslConfig.getTrustStorePath()).getInputStream();
+
+			final KeyStore trustStore = KeyStore.getInstance(sslConfig
+					.getTrustStoreType());
+			trustStore.load(trustStoreIS, sslConfig.getTrustStorePassword()
+					.toCharArray());
+
+			final TrustManagerFactory trustManagerFactory = TrustManagerFactory
+					.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+			 trustManagerFactory.init(trustStore);
+//			trustManagerFactory.init((KeyStore) null);
+			trustManagers = trustManagerFactory.getTrustManagers();
+		}
 
 		final SSLContext context = SSLContext.getInstance(PROTOCOL_VERSION);
 		context.init(keyManagers, trustManagers, null);
