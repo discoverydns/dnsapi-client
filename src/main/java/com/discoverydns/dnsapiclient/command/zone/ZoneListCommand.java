f3a730138501ff3f38a0af2ae4dba85519a897d6
Chris Wright <chris@ausregistry.com.au>
2014-02-17 11:28:34 +1100

    fixed somehting

--- src/main/java/com/discoverydns/dnsapiclient/command/zone/ZoneListCommand.java
+++ src/main/java/com/discoverydns/dnsapiclient/command/zone/ZoneListCommand.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonRootName;
  * @author Chris Wright
  */
 @JsonRootName("ZoneListCommand")
-@JsonPropertyOrder({ "searchName", "searchGroup",
+@JsonPropertyOrder({ "searchName", "searchNameSearchType","searchGroup",
 		"searchNameServerInterfaceSetId", "searchNameServerSetId",
 		"searchBrandedNameServers", "searchDNSSECSigned" })
 public class ZoneListCommand {
