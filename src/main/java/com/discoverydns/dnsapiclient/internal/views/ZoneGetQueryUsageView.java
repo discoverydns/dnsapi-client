8b512ed2c81b15bba4c7d6c385a2ca0a4a43d009
Arnaud Dumont <arnaud.dumont@ausregistry.com.au>
2014-06-17 11:36:18 +1000

    Arnaud: Reverted ZoneGetQueryUsageView fields name changes, for backward compatibility

--- src/main/java/com/discoverydns/dnsapiclient/internal/views/ZoneGetQueryUsageView.java
+++ src/main/java/com/discoverydns/dnsapiclient/internal/views/ZoneGetQueryUsageView.java
@@ -7,11 +7,11 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 import java.net.URI;
 import java.util.List;
 
-@JsonRootName("queryUsage")
+@JsonRootName("zoneGetQueryUsage")
 public class ZoneGetQueryUsageView {
     @JsonProperty("@uri")
     private URI uri;
-    @JsonProperty("queryUsageRecords")
+    @JsonProperty("zoneQueryUsageRecords")
     public List<ZoneQueryUsageRecord> zoneQueryUsageRecords;
     @JsonProperty("totalRecordCount")
     public long totalRecordCount;
