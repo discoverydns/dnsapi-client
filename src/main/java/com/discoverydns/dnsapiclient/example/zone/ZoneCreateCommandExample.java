ed494709d9af4f0253c9698a24ef8598fe23e913
Arnaud Dumont <arnaud.dumont@ausregistry.com.au>
2014-08-11 16:28:37 +1000

    Provide user resource records on Zone Create

--- src/main/java/com/discoverydns/dnsapiclient/example/zone/ZoneCreateCommandExample.java
+++ src/main/java/com/discoverydns/dnsapiclient/example/zone/ZoneCreateCommandExample.java
@@ -1,5 +1,12 @@
 package com.discoverydns.dnsapiclient.example.zone;
 
+import java.net.InetAddress;
+
+import org.xbill.DNS.ARecord;
+import org.xbill.DNS.DClass;
+import org.xbill.DNS.Name;
+import org.xbill.DNS.Record;
+
 import ch.qos.logback.classic.Level;
 import com.discoverydns.dnsapiclient.DNSAPIClient;
 import com.discoverydns.dnsapiclient.DNSAPIClientFactory;
@@ -12,6 +19,7 @@ import com.discoverydns.dnsapiclient.config.DNSAPIClientConfig;
 import com.discoverydns.dnsapiclient.example.ExampleDNSAPIClientConfig;
 import com.discoverydns.dnsapiclient.example.ExampleDefaultSSLContextFactoryConfig;
 import com.discoverydns.dnsapiclient.example.ExampleDefaultTransactionLogHandlerConfig;
+import com.google.common.collect.Sets;
 
 /**
  * Example of sending an ZoneCreateCommand and receiving an ZoneCreateResponse
@@ -41,15 +49,24 @@ public class ZoneCreateCommandExample {
         }
 
         //Create command
-        final ZoneCreateCommand command =
-                new ZoneCreateCommand.Builder()
+        ZoneCreateCommand command = null;
+        try {
+            command = new ZoneCreateCommand.Builder()
                         .withName("myzone.com")
                         .withDnssecSigned(Boolean.FALSE)
                         .withBrandedNameServers(Boolean.TRUE)
                         .withGroup("mygroup")
                         .withNameServerSetId("<my-nss-id>")
                         .withPlanId("<my-plan-id>")
+                        .withResourceRecords(
+                                Sets.newHashSet(
+                                        (Record) new ARecord(Name.fromString("myzone.com."), DClass.IN, 3600,
+                                                InetAddress.getByName("1.2.3.4"))))
                         .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
         //Send command to server and receive response
         try {
