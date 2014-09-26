03dc34fe6b2cbed98b75e6ba317ed1f50d104f64
rcane <richardc@ausregistry.com.au>
2014-09-26 12:35:28 +1000

    Display TSIG in Rest

--- src/main/java/com/discoverydns/dnsapiclient/command/zone/ZoneGetResponse.java
+++ src/main/java/com/discoverydns/dnsapiclient/command/zone/ZoneGetResponse.java
@@ -1,6 +1,7 @@
 package com.discoverydns.dnsapiclient.command.zone;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Set;
 
 import org.joda.time.LocalDateTime;
@@ -16,105 +17,114 @@ import com.fasterxml.jackson.annotation.JsonRootName;
  * Response object received by the {@link com.discoverydns.dnsapiclient.DNSAPIClient}
  * from the DNSAPI server subsequently to the sending of a {@link ZoneGetCommand},
  * describing the details of the retrieved Zone.
- *
+ * <p/>
  * A Zone, belonging to an Account, will be managed by the DNSAPI architecture
  * for domain names resolution purpose.
  *
  * @author Chris Wright
  */
 @JsonRootName("ZoneGetResponse")
-@JsonPropertyOrder({ "@uri", "id", "version", "name", "serial",
-		"brandedNameServers", "dnssecSigned", "zskRollOverState",
+@JsonPropertyOrder({"@uri", "id", "version", "name", "serial",
+        "brandedNameServers", "dnssecSigned", "zskRollOverState",
         "pendingOperation", "lastPublishDate",
         "nameServerSetId", "nameServerSetName", "nameServerInterfaceSetId",
-		"nameServerInterfaceSetName", "planId", "planName", "group",
-		"sponsorAccountId", "sponsorAccountIdentifier", "createDate",
-		"createAccountId", "createAccountIdentifier", "createUserId",
-		"createUserName", "lastUpdateDate", "lastUpdateAccountId",
-		"lastUpdateAccountIdentifier", "lastUpdateUserId",
-		"lastUpdateUserName", "delegationResourceRecords",
-        "ddnsResourceRecords", "resourceRecords" })
+        "nameServerInterfaceSetName", "planId", "planName", "group",
+        "sponsorAccountId", "sponsorAccountIdentifier", "createDate",
+        "createAccountId", "createAccountIdentifier", "createUserId",
+        "createUserName", "lastUpdateDate", "lastUpdateAccountId",
+        "lastUpdateAccountIdentifier", "lastUpdateUserId",
+        "lastUpdateUserName", "delegationResourceRecords",
+        "ddnsResourceRecords", "resourceRecords", "axfrEnabled", "axfrServers",
+        "axfrTransferDate", "axfrTransferServer", "axfrAttemptedTransferDate",
+        "axfrAttemptedTransferServer" })
 public class ZoneGetResponse {
 
-	@JsonIgnore
-	private final ZoneGetView zoneGetView;
+    @JsonIgnore
+    private final ZoneGetView zoneGetView;
 
-	public ZoneGetResponse(final ZoneGetView zoneGetView) {
-		this.zoneGetView = zoneGetView;
-	}
+    public ZoneGetResponse(final ZoneGetView zoneGetView) {
+        this.zoneGetView = zoneGetView;
+    }
 
     /**
      * @return The URI representing the retrieved Zone on the DNSAPI server
      */
-	@JsonProperty("@uri")
-	public URI getURI() {
-		return zoneGetView.getUri();
-	}
+    @JsonProperty("@uri")
+    public URI getURI() {
+        return zoneGetView.getUri();
+    }
 
     /**
      * @return The Zone UUID
      */
-	@JsonProperty("id")
-	public String getId() {
-		return zoneGetView.getId();
-	}
+    @JsonProperty("id")
+    public String getId() {
+        return zoneGetView.getId();
+    }
 
     /**
      * @return The Zone modification version
      */
-	@JsonProperty("version")
-	public Long getVersion() {
-		return zoneGetView.getVersion();
-	}
+    @JsonProperty("version")
+    public Long getVersion() {
+        return zoneGetView.getVersion();
+    }
 
     /**
      * @return The Zone name
      */
-	@JsonProperty("name")
-	public String getName() {
-		return zoneGetView.getName();
-	}
+    @JsonProperty("name")
+    public String getName() {
+        return zoneGetView.getName();
+    }
 
     /**
-     * @return true if the Zone uses the branded nameServers {@link com.discoverydns.dnsapiclient.command.plan.PlanFeature}, false otherwise
+     * @return true if the Zone uses the branded nameServers
+     * {@link com.discoverydns.dnsapiclient.command.plan.PlanFeature}, false otherwise
      */
-	@JsonProperty("brandedNameServers")
-	public Boolean getBrandedNameServers() {
-		return zoneGetView.getBrandedNameServers();
-	}
+    @JsonProperty("brandedNameServers")
+    public Boolean getBrandedNameServers() {
+        return zoneGetView.getBrandedNameServers();
+    }
 
     /**
      * @return The list of system-generated Resource {@link Record}s of the Zone
      */
-	@JsonProperty("ddnsResourceRecords")
-	public Set<Record> getDDNSResourceRecords() {
-		return zoneGetView.getDDNSResourceRecords();
-	}
+    @JsonProperty("ddnsResourceRecords")
+    public Set<Record> getDDNSResourceRecords() {
+        return zoneGetView.getDDNSResourceRecords();
+    }
 
     /**
      * @return The list of system-generated delegation Resource {@link Record}s of the Zone
      */
-	@JsonProperty("delegationResourceRecords")
-	public Set<Record> getDelegationResourceRecords() {
-		return zoneGetView.getDelegationResourceRecords();
-	}
+    @JsonProperty("delegationResourceRecords")
+    public Set<Record> getDelegationResourceRecords() {
+        return zoneGetView.getDelegationResourceRecords();
+    }
 
     /**
-     * @return true if the zone uses the DNSSEC signing {@link com.discoverydns.dnsapiclient.command.plan.PlanFeature}, false otherwise
+     * @return true if the zone uses the DNSSEC signing
+     * {@link com.discoverydns.dnsapiclient.command.plan.PlanFeature}, false otherwise
      */
-	@JsonProperty("dnssecSigned")
-	public Boolean getDNSSECSigned() {
-		return zoneGetView.getDNSSECSigned();
-	}
+    @JsonProperty("dnssecSigned")
+    public Boolean getDNSSECSigned() {
+        return zoneGetView.getDNSSECSigned();
+    }
 
     /**
      * This returns the state of the zone in the ZSK roll over process.
-     * This process enables the zone's Zone Signing Keys to be replaced after a certain period of time, for security purpose.
+     * This process enables the zone's Zone Signing Keys to be replaced after a certain period of time,
+     * for security purpose.
      * The value returned can either be:
-     *     - "scheduled": the process has not started yet, but will start on "zskNextActionDate".
-     *     - "stage1": the new ZSK has been generated and added to the zone, but will only be used on "zskNextActionDate".
-     *     - "stage2": the new ZSK is now in use and the old one is not anymore. The latter will be definitely deleted on "zskNextActionDate".
-     * @return The state of the zone in the ZSK roll over process ("scheduled", "stage1" or "stage2"), if the zone is DNSSEC-signed (null otherwise)
+     * - "scheduled": the process has not started yet, but will start on "zskNextActionDate".
+     * - "stage1": the new ZSK has been generated and added to the zone, but will only be used on
+     * "zskNextActionDate".
+     * - "stage2": the new ZSK is now in use and the old one is not anymore. The latter will be definitely
+     * deleted on "zskNextActionDate".
+     *
+     * @return The state of the zone in the ZSK roll over process ("scheduled", "stage1" or "stage2"), if the zone
+     * is DNSSEC-signed (null otherwise)
      */
     @JsonProperty("zskRollOverState")
     public String getZskRollOverState() {
@@ -122,7 +132,8 @@ public class ZoneGetResponse {
     }
 
     /**
-     * @return The pending operation that will soon be actioned on the zone ('zoneSigning', 'zoneUnSigning' or 'delete'), or 'none'
+     * @return The pending operation that will soon be actioned on the zone ('zoneSigning',
+     * 'zoneUnSigning' or 'delete'), or 'none'
      */
     @JsonProperty("pendingOperation")
     public String getPendingOperation() {
@@ -135,175 +146,246 @@ public class ZoneGetResponse {
     @JsonProperty("lastPublishDate")
     public LocalDateTime getLastPublishDate() {
         return zoneGetView.getLastPublishDate();
-	}
+    }
 
     /**
      * @return The UUID of the NameServer Interface Set this Zone is associated with
      */
-	@JsonProperty("nameServerInterfaceSetId")
-	public String getNameServerInterfaceSetId() {
-		return zoneGetView.getNameServerInterfaceSetId();
-	}
+    @JsonProperty("nameServerInterfaceSetId")
+    public String getNameServerInterfaceSetId() {
+        return zoneGetView.getNameServerInterfaceSetId();
+    }
 
     /**
      * @return The name of the NameServer Interface Set this Zone is associated with
      */
-	@JsonProperty("nameServerInterfaceSetName")
-	public String getNameServerInterfaceSetName() {
-		return zoneGetView.getNameServerInterfaceSetName();
-	}
+    @JsonProperty("nameServerInterfaceSetName")
+    public String getNameServerInterfaceSetName() {
+        return zoneGetView.getNameServerInterfaceSetName();
+    }
 
     /**
      * @return The UUID of the NameServer Set this Zone is associated with
      */
-	@JsonProperty("nameServerSetId")
-	public String getNameServerSetId() {
-		return zoneGetView.getNameServerSetId();
-	}
+    @JsonProperty("nameServerSetId")
+    public String getNameServerSetId() {
+        return zoneGetView.getNameServerSetId();
+    }
 
     /**
      * @return The name of the NameServer Set this Zone is associated with
      */
-	@JsonProperty("nameServerSetName")
-	public String getNameServerSetName() {
-		return zoneGetView.getNameServerSetName();
-	}
+    @JsonProperty("nameServerSetName")
+    public String getNameServerSetName() {
+        return zoneGetView.getNameServerSetName();
+    }
 
     /**
      * @return The UUID of the Plan this Zone is using
      */
-	@JsonProperty("planId")
-	public String getPlanId() {
-		return zoneGetView.getPlanId();
-	}
+    @JsonProperty("planId")
+    public String getPlanId() {
+        return zoneGetView.getPlanId();
+    }
 
     /**
      * @return The name of the Plan this Zone is using
      */
-	@JsonProperty("planName")
-	public String getPlanName() {
-		return zoneGetView.getPlanName();
-	}
+    @JsonProperty("planName")
+    public String getPlanName() {
+        return zoneGetView.getPlanName();
+    }
 
     /**
      * @return The name of the group this Zone belongs to,
      * in case it's using the "grouping" {@link com.discoverydns.dnsapiclient.command.plan.PlanFeature}.
      */
-	@JsonProperty("group")
-	public String getGroup() {
-		return zoneGetView.getGroup();
-	}
+    @JsonProperty("group")
+    public String getGroup() {
+        return zoneGetView.getGroup();
+    }
 
     /**
      * @return The Zone serial number
      */
-	@JsonProperty("serial")
-	public Long getSerial() {
-		return zoneGetView.getSerial();
-	}
+    @JsonProperty("serial")
+    public Long getSerial() {
+        return zoneGetView.getSerial();
+    }
 
     /**
      * @return The list of user supplied (and modifiable) Resource {@link Record}s of the Zone
      */
-	@JsonProperty("resourceRecords")
-	public Set<Record> getUserResourceRecords() {
-		return zoneGetView.getUserResourceRecords();
-	}
+    @JsonProperty("resourceRecords")
+    public Set<Record> getUserResourceRecords() {
+        return zoneGetView.getUserResourceRecords();
+    }
 
     /**
      * @return The UUID of the Account who sponsors this Zone
      */
-	@JsonProperty("sponsorAccountId")
-	public String getSponsorAccountId() {
-		return zoneGetView.getSponsorAccountId();
-	}
+    @JsonProperty("sponsorAccountId")
+    public String getSponsorAccountId() {
+        return zoneGetView.getSponsorAccountId();
+    }
 
     /**
      * @return The unique identifier of the Account who sponsors this Zone
      */
-	@JsonProperty("sponsorAccountIdentifier")
-	public String getSponsorAccountIdentifier() {
-		return zoneGetView.getSponsorAccountIdentifier();
-	}
+    @JsonProperty("sponsorAccountIdentifier")
+    public String getSponsorAccountIdentifier() {
+        return zoneGetView.getSponsorAccountIdentifier();
+    }
 
     /**
      * @return The UUID of the Account who created this Zone
      */
-	@JsonProperty("createAccountId")
-	public String getCreateAccountId() {
-		return zoneGetView.getCreateAccountId();
-	}
+    @JsonProperty("createAccountId")
+    public String getCreateAccountId() {
+        return zoneGetView.getCreateAccountId();
+    }
 
     /**
      * @return The unique identifier of the Account who created this Zone
      */
-	@JsonProperty("createAccountIdentifier")
-	public String getCreateAccountIdentifier() {
-		return zoneGetView.getCreateAccountIdentifier();
-	}
+    @JsonProperty("createAccountIdentifier")
+    public String getCreateAccountIdentifier() {
+        return zoneGetView.getCreateAccountIdentifier();
+    }
 
     /**
      * @return The UUID of the User who created this Zone
      */
-	@JsonProperty("createUserId")
-	public String getCreateUserId() {
-		return zoneGetView.getCreateUserId();
-	}
+    @JsonProperty("createUserId")
+    public String getCreateUserId() {
+        return zoneGetView.getCreateUserId();
+    }
 
     /**
      * @return The name of the User who created this Zone
      */
-	@JsonProperty("createUserName")
-	public String getCreateUserName() {
-		return zoneGetView.getCreateUserName();
-	}
+    @JsonProperty("createUserName")
+    public String getCreateUserName() {
+        return zoneGetView.getCreateUserName();
+    }
 
     /**
      * @return The creation date of this Zone
      */
-	@JsonProperty("createDate")
-	public LocalDateTime getCreateDate() {
-		return zoneGetView.getCreateDate();
-	}
+    @JsonProperty("createDate")
+    public LocalDateTime getCreateDate() {
+        return zoneGetView.getCreateDate();
+    }
 
     /**
      * @return The UUID of the Account who last updated this Zone
      */
-	@JsonProperty("lastUpdateAccountId")
-	public String getLastUpdateAccountId() {
-		return zoneGetView.getLastUpdateAccountId();
-	}
+    @JsonProperty("lastUpdateAccountId")
+    public String getLastUpdateAccountId() {
+        return zoneGetView.getLastUpdateAccountId();
+    }
 
     /**
      * @return The unique identifier of the Account who last updated this Zone
      */
-	@JsonProperty("lastUpdateAccountIdentifier")
-	public String getLastUpdateAccountIdentifier() {
-		return zoneGetView.getLastUpdateAccountIdentifier();
-	}
+    @JsonProperty("lastUpdateAccountIdentifier")
+    public String getLastUpdateAccountIdentifier() {
+        return zoneGetView.getLastUpdateAccountIdentifier();
+    }
 
     /**
      * @return The UUID of the User who last updated this Zone
      */
-	@JsonProperty("lastUpdateUserId")
-	public String getLastUpdateUserId() {
-		return zoneGetView.getLastUpdateUserId();
-	}
+    @JsonProperty("lastUpdateUserId")
+    public String getLastUpdateUserId() {
+        return zoneGetView.getLastUpdateUserId();
+    }
 
     /**
      * @return The name of the User who last updated this Zone
      */
-	@JsonProperty("lastUpdateUserName")
-	public String getLastUpdateUserName() {
-		return zoneGetView.getLastUpdateUserName();
-	}
+    @JsonProperty("lastUpdateUserName")
+    public String getLastUpdateUserName() {
+        return zoneGetView.getLastUpdateUserName();
+    }
 
     /**
      * @return The last update date of this Zone
      */
-	@JsonProperty("lastUpdateDate")
-	public LocalDateTime getLastUpdateDate() {
-		return zoneGetView.getLastUpdateDate();
-	}
+    @JsonProperty("lastUpdateDate")
+    public LocalDateTime getLastUpdateDate() {
+        return zoneGetView.getLastUpdateDate();
+    }
 
+    /**
+     * @return This is an AXFR enabled zone
+     */
+    @JsonProperty("axfrEnabled")
+    public Boolean getAxfrEnabled() {
+        return zoneGetView.getAxfrEnabled();
+    }
+
+    /**
+     * @return List of servers to interrogate to do an AXFR transfer
+     */
+    @JsonProperty("axfrServers")
+    public List<String> getAxfrServers() {
+        return zoneGetView.getAxfrServers();
+    }
+
+    /**
+     * @return The date of a the last successful AXFR transfer
+     */
+    @JsonProperty("axfrTransferDate")
+    public LocalDateTime getAxfrTransferDate() {
+        return zoneGetView.getAxfrTransferDate();
+    }
+
+    /**
+     * @return The server used for the last successful AXFR transfer
+     */
+    @JsonProperty("axfrTransferServer")
+    public String getAxfrTransferServer() {
+        return zoneGetView.getAxfrTransferServer();
+    }
+
+    /**
+     * @return The date of a the last attempted AXFR transfer. Only set on unsuccessful transfer.
+     */
+    @JsonProperty("axfrAttemptedTransferDate")
+    public LocalDateTime getAxfrAttemptedTransferDate() {
+        return zoneGetView.getAxfrAttemptedTransferDate();
+    }
+
+    /**
+     * @return The server used for the last attempted AXFR transfer. Only set on unsuccessful transfer.
+     */
+    @JsonProperty("axfrAttemptedTransferServer")
+    public String getAxfrAttemptedTransferServer() {
+        return zoneGetView.getAxfrAttemptedTransferServer();
+    }
+
+    /**
+     * @return The name component of axfr TSIG.
+     */
+    @JsonProperty("axfrTsigName")
+    public String getAxfrTsigName() {
+        return zoneGetView.getAxfrTsigName();
+    }
+
+    /**
+     * @return The algorithm component of axfr TSIG.
+     */
+    @JsonProperty("axfrTsigAlgorithm")
+    public String getAxfrTsigAlgorithm() {
+        return zoneGetView.getAxfrTsigAlgorithm();
+    }
+
+    /**
+     * @return The key component of axfr TSIG.
+     */
+    @JsonProperty("axfrTsigKey")
+    public String getAxfrTsigKey() {
+        return zoneGetView.getAxfrTsigKey();
+    }
 }
